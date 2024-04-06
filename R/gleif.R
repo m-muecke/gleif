b495ba5350227e75cfed2c531d6d7a06f2b71b1e
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-04-06 19:14:01 +0200

    feat: support for download all available lei mapping files

---
+++ R/gleif.R
@@ -0,0 +1,34 @@
+latest_url <- function(type = c("isin", "bic", "mic", "oc")) {
+  type <- match.arg(type)
+  # nolint start
+  url <- switch(type,
+    isin = "https://www.gleif.org/en/lei-data/lei-mapping/download-isin-to-lei-relationship-files",
+    bic = "https://www.gleif.org/en/lei-data/lei-mapping/download-bic-to-lei-relationship-files",
+    mic = "https://www.gleif.org/en/lei-data/lei-mapping/download-mic-to-lei-relationship-files",
+    oc = "https://www.gleif.org/en/lei-data/lei-mapping/download-oc-to-lei-relationship-files"
+  )
+  # nolint end
+  files <- rvest::read_html(url) |>
+    rvest::html_element("table") |>
+    rvest::html_elements("a") |>
+    rvest::html_attr("href")
+  files[[1L]]
+}
+
+gleif_download <- function(url) {
+  tmp <- tempfile()
+  dir.create(tmp)
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  tf <- file.path(tmp, "tempfile.zip")
+  utils::download.file(url, destfile = tf, quiet = TRUE, mode = "wb")
+  file <- utils::unzip(tf, exdir = tmp)
+  mapping <- read.csv(file)
+  setNames(mapping, tolower(names(mapping)))
+}
+
+#' @export
+lei_mapping <- function(type = c("isin", "bic", "mic", "oc")) {
+  url <- latest_url(type)
+  mapping <- gleif_download(url)
+  as_tibble(mapping)
+}
